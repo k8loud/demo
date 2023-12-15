f84352a20c30393fd40c5b9cbe93bd9028eaa5d7
Agwen12 <pawel.pycia29@gmail.com>
2023-12-16 00:37:24 +0100

    adjusting for comments

--- scenarios/k8s-scaling/run.sh
+++ scenarios/k8s-scaling/run.sh
@@ -1,10 +1,10 @@
 # shellcheck disable=SC2046
-echo $(date +"%Y-%m-%d %T") start test
+echo "$(date +'%Y-%m-%d %T') start test"
 
 curr=0
 for ((i=1; i<=6; i++)); do
     ((curr++))
-    echo "$(date +"%Y-%m-%d %T") Scaling loadtest to $curr"
+    echo "$(date +'%Y-%m-%d %T') Scaling loadtest to $curr"
     kubectl scale deploy load-test -n loadtest --replicas="$curr"
 
 
@@ -14,5 +14,4 @@ sleep 240
 echo scaling down
 kubectl scale deploy load-test -n loadtest --replicas=0
 sleep 60
-echo $(date +"%Y-%m-%d %T") test finished
-
+echo "$(date +'%Y-%m-%d %T') test finished"
